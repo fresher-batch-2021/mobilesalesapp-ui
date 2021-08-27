a4a7ec8f490870d9f3a7f381d452b13f24dd5af4
Bharathi kannan <baranibk13299@gmail.com>
2021-08-27 16:09:42 +0530

    Added service apiA

---
+++ service-api/services/validator.js
@@ -0,0 +1,19 @@
+class Validator {
+  static isValidString(input, message) {
+    console.log("input", input)
+    if (input == null|| input.trim() == "" ) {
+      throw new Error(message);
+    }
+  }
+  static isValidPassword(input, message) {
+    if (input == "" || input == null || input.trim() == "") {
+      throw new Error(message);
+    }
+
+  }
+  static isValidPasswordStrength(input, message) {
+    if (input.length <= 6) {
+      throw new Error(message);
+    }
+  }
+}
